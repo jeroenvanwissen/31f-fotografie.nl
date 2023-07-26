8187463937f52f127e41425bbe0d0e6c65c517d4
Jeroen van Wissen <jeroen.van.wissen@gmail.com>
2023-07-26 22:56:16 +0200

    Updates

---
+++ .tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,105 @@
+// .tina/config.ts
+import { defineConfig } from "tinacms";
+var branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+var config_default = defineConfig({
+  branch,
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID || "",
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN || "",
+  // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public"
+  },
+  media: {
+    tina: {
+      mediaRoot: "photos",
+      publicFolder: "public"
+    }
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/content/post",
+        fields: [
+          {
+            label: "Draft",
+            name: "draft",
+            type: "boolean"
+          },
+          {
+            label: "Date",
+            name: "date",
+            type: "datetime",
+            ui: {
+              timeFormat: "HH:mm"
+            }
+          },
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "string",
+            name: "categories",
+            label: "Categories",
+            list: true
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description"
+          },
+          {
+            label: "Image",
+            name: "image",
+            type: "object",
+            fields: [
+              {
+                label: "Image src",
+                name: "url",
+                type: "image"
+              },
+              {
+                label: "Image Alt",
+                name: "alt",
+                type: "string"
+              }
+            ]
+          },
+          {
+            label: "Socials",
+            name: "socials",
+            type: "object",
+            fields: [
+              {
+                label: "Instagram",
+                name: "instagram",
+                type: "string"
+              },
+              {
+                label: "Mastodon",
+                name: "mastodon",
+                type: "string"
+              }
+            ]
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
